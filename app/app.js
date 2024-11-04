a6dd57820936341c97d32d60bb51035b81fcd01e
joshhwuu <joshuawu2004@gmail.com>
2024-11-03 16:49:02 -0800

    express/node skeletons, database setup

---
+++ app/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import db from "./db.js"; 
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", async (req, res) => {
+  const query = "SELECT * FROM public.pokemon WHERE id = 73";
+  try {
+    const result = await db.query(query);
+    res.send(result.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
