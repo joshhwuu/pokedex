b6be89830bd59d15fc27603f271670c4c24d86f7
Matt <73326282+MattWanJH@users.noreply.github.com>
2024-11-28 23:09:38 -0800

    fixed minor bug

--- app/backend/db.js
+++ app/backend/db.js
@@ -6,13 +6,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "local",
-  password: "postgres",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
-
 
 const query = (text, params) => pool.query(text, params);
 
